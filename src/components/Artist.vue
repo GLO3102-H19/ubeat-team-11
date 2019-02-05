488af89bad2a64b7d54a2f2ba37d544aa5c83b78
JacobGeoGeek <jacobchaar@live.ca>
2019-02-05 00:18:40 -0500

    Progress of designing the Artist template

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,18 +1,25 @@
 <template>
     <div>
-          <div class="artistBanner">
+    <div class="artistBanner">
      <h4 class="artistName">Artist Name</h4>
      <p class="artistDescription">
          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quis 
          adipisci culpa nihil sed corporis assumenda enim ex hic sint odio, tempora vero omnis fuga aliquid necessitatibus, 
          exercitationem repudiandae ducimus.
      </p>
-    </div>
 
+     <button class="randomSong">
+         Shuffle
+     </button>
     </div>
-    
- 
-  
+    <div class="artistContent">
+        <h4 class="artistContentSubTitles">Popular Songs</h4>
+        <div class="artistPopularSont">Add something...</div>
+        <h4 class="artistContentSubTitles">Albums</h4>
+        <div class="artistAlbum">Add somethins...</div>
+    </div>
+    </div>
+     
 </template>
 
 <style>
@@ -45,10 +52,31 @@ width: 100%;
     font-size: 16px; 
     line-height: 26px; 
     margin: 0;
-    text-align: inherit; 
-    text-indent: 25px;
-    text-justify: newspaper;
+    padding-left: 2%;
+    padding-right: 50%;
+    text-align:justify; 
+    text-indent: 30px;
+    
    
+}
+.randomSong{
+    background: whitesmoke;
+    border: 2px solid #fff;
+    font-family: 'Helvetica',sans-serif;
+    font-size: 10px;
+    font-weight: 700;
+    letter-spacing: 1px;
+    margin: 8px 15px;
+    overflow: hidden; /*Pour une icone Random*/
+    padding: 10px 30px;
+    text-transform: uppercase;
+    transition: all 0.3s;
+    width: 25%;
+  
+
+}
+.randomSong:hover{
+  
 }
 
 </style>
