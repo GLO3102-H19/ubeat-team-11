421e22931b694cace4e1bf276734340d6a0d463d
JacobGeoGeek <jacobchaar@live.ca>
2019-02-05 17:18:57 -0500

    Begin the Design of the NavBar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,33 @@
 <template> 
     <nav>
-        <ul>
+        <ul class="navBarContainer">
             <li>
                 <router-link to="/">Home</router-link>
             </li>
             <li>
-                <router-link to="/album">Album</router-link>
-            </li>
-            <li>
                 <router-link to="/artist">Artist</router-link>
             </li>
+            <li>
+                <router-link to="/album">Album</router-link>
+            </li>
+            <li class="search">
+                <input type="text" class="searchInput" placeholder="Search...">
+            </li>
+
         </ul>
     </nav>
 </template>
 
 <style>
+.navBarContainer{
+    display: flex;
+    justify-content:space-around;
+}
+.searchInput{
+    flex:1;
+}
+.navBarContainer>li{
+    flex:1;
+}
+
 </style>
