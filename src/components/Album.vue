7c6cd809be3f9dc78fa3d360dc49d73c9080d51a
Koriama <akoriama@gmail.com>
2019-02-09 00:12:23 -0500

    Modification dans la vue Album

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,53 +1,135 @@
 <template>
-    <div id="Album">
+    <div id="album">
 
-      <div class="Album">
 
-        <md-card style="background-color: black">
-            <md-card-header >
-              <md-card-header-text >
-                <div class="md-title" style="color: white"  >Album</div>
-                <div class="md-subhead" style="color: white" >Artist</div>
-                <p class="albumDescription">  Studio album by the Beatles                       </p>
-                <p class="albumDescription">  Released	26 September 1969                       </p>
-                <p class="albumDescription">  Recorded	22 February – 20 August 1969            </p>
-                <p class="albumDescription">  Length	47:23                                     </p>
-               </md-card-header-text>
+       <md-content style="background-color: black;color: white">
+              <div class="imageAlbum" >
+                  <figure>
+                     <img src="https://dummyimage.com/200x200/4558ff/000000.jpg&text=Album" alt="Album">
+                     <figcaption  >Album name </figcaption>
+                     <md-button > <md-icon style="color: white">shuffle </md-icon></md-button>
+                   </figure>
+                 <p class= "textAlbum"  >
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    In quod, cum eum beatae aperiam nobis laudantium necessitatibus
+                    perferendis,
+                    laboriosam natus dignissimos reprehenderit suscipit deserunt eos eligendi,
+                    perspiciatis molestiae sit minima.
+                  </p>
+              </div>
+      </md-content>
 
-              <md-card-media-big class="imageAlbum" >
-                <img class="img" align="left" width="250" height="250" src="https://upload.wikimedia.org/wikipedia/pt/3/3d/Abbey_Road.jpg" alt="Album Pic">
-              </md-card-media-big>
-            </md-card-header>
 
-            <md-card-actions class="buttonAlbum" >
-              <md-button >  <md-icon>fast_rewind</md-icon>   </md-button>
-              <md-button >  <md-icon>play_arrow</md-icon>   </md-button>
-              <md-button >  <md-icon>fast_forward</md-icon>   </md-button>
-            </md-card-actions>
-         </md-card>
 
+      <div class="artistContent">
+        <md-table md-card>
+
+
+          <md-table-row style="background:black">
+            <md-table-head style="color:white">Track</md-table-head>
+            <md-table-head style="color: white">Title</md-table-head>
+            <md-table-head style="color:white">Time</md-table-head>
+            <md-table-head style="color:white"> </md-table-head>
+          </md-table-row>
+
+          <md-table-row class="song">
+            <md-table-cell>1</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+
+
+          </md-table-row><md-table-row class="song">
+            <md-table-cell>2</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+        </md-table-row>
+
+          <md-table-row class="song">
+            <md-table-cell>3</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+
+          </md-table-row><md-table-row class="song">
+            <md-table-cell>4</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+        </md-table-row>
+
+          <md-table-row class="song">
+            <md-table-cell>5</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+          </md-table-row><md-table-row class="song">
+            <md-table-cell>6</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+           </md-table-row>
+
+          <md-table-row class="song">
+            <md-table-cell>7</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+          </md-table-row><md-table-row class="song">
+            <md-table-cell>8</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+           </md-table-row>
+
+
+          </md-table-row><md-table-row class="song">
+            <md-table-cell>9</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+           </md-table-row>
+
+          <md-table-row class="song">
+            <md-table-cell>10</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+          </md-table-row><md-table-row class="song">
+            <md-table-cell>11</md-table-cell>
+            <md-table-cell>Beta</md-table-cell>
+            <md-table-cell>1:47</md-table-cell>
+            <md-table-cell><md-icon style="color: red">play_arrow</md-icon></md-table-cell>
+
+           </md-table-row>
+
+        </md-table>
       </div>
 
-      <div class="song" > <h3>song 1</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 2</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 3</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 4</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 5</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 6</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 7</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 8</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
-      <div class="song"> <h3>song 9</h3> <md-button class="md-accent" align="top">  <md-icon>play_arrow</md-icon></md-button> </div>
+
 
 
     </div>
+
+
 </template>
 
 <style>
 
 
 
-
-  #Album
+  #album
   {
     background-size: cover;
     background-color: black;
@@ -58,46 +140,26 @@
   }
 
 
-  .albumDescription{
-    color: #bbc3c8;
-    font-family: Verdana sans-serif;
-    font-size: 16px;
-    line-height: 26px;
-    margin: 0;
-    position: relative;
-    padding-left: 2%;
-    padding-right: 50%;
-    text-align:justify;
-    text-indent: 30px;
-
-  }
-
-  .buttonAlbum{
-    color: black;
-    position: page;
-    padding-left: 2%;
-    padding-right: 30%;
-
-  }
   .imageAlbum{
-    justify-content: revert;
-    position: page;
-    padding-left: 2%;
-    padding-right: 30%;
+    align-content:space-evenly;
+   display: flex;
+
   }
 
-
-  .md-accent {
-    color: white;
-  }
 
   .song{
+    background-color: black;
     color: white;
-    display: flex;
-    height: unset;
-    justify-content: space-around;
-    width:250px;
-  }
 
+  }
+.textAlbum{
+  align-self:center;
+  justify-content: center;
+  text-align: justify;
+  width: 40%;
+
+;
+
+}
 
 </style>
