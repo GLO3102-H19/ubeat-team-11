e3452d5684dbaecbd23b1f43a2bc6500574b8e17
JacobGeoGeek <jacobchaar@live.ca>
2019-02-08 19:54:00 -0500

    Edit the Navigation we need something for the barvar to be more responsive

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,50 +1,57 @@
 <template>
-   
-    <div>
-     
-
-      <md-toolbar class="md-default" md-elevation="1">
-        <h3 class="md-title md-xsmall-hide md-small-hide" style="flex: 1">Ubeat</h3>
-        <md-button class="md-xsmall-hide md-small-hide" to="/">Home</md-button>
-        <md-button class="md-xsmall-hide md-small-hide" to="/artist">Artist</md-button>
-        <md-button class="md-xsmall-hide md-small-hide" to="/Album">Album</md-button>
- 
-
 
     
+    <md-toolbar>
       <div class="md-toolbar-row">
-        <div class="md-toolbar-section-start ">
-          <md-button class="md-icon-button">
-            <md-icon class="md-medium-hide md-large-hide md-xlarge-hide">menu</md-icon>
-          </md-button>
+        <div class="md-toolbar-section-start">
+         <h3 class="md-title" style="flex: 1">Ubeat</h3>
         </div>
+
+        <md-autocomplete
+        
+          class="search"
+          v-model="selectSong"
+          :md-options="songs"
+          md-layout="box">
+          <label style="color: black">Search...</label>
+        </md-autocomplete>
 
         <div class="md-toolbar-section-end">
-          <md-button class="md-icon-button">
-            <md-icon class="md-medium-hide md-large-hide md-xlarge-hide">refresh</md-icon>
-          </md-button>
 
-          <md-button class="md-icon-button">
-            <md-icon class="md-medium-hide md-large-hide md-xlarge-hide">more_vert</md-icon>
-          </md-button>
+           <md-button to="/artist">Artist</md-button>
+      <md-button to="/album">Album</md-button>
+         
         </div>
       </div>
-
-    
     </md-toolbar>
-
-
-
-
-
-    </div>
-
-    
-    
+   
+  
 </template>
-
+<script>
+  export default {
+    name: 'AutocompleteBox',
+    data: () => ({
+      selectSong: null,
+      songs: [
+        'Alpha',
+        'Beta',
+        'Charlie',
+        'Delta',
+        'Echo'
+      ]
+    })
+  };
+</script>
 
 <style>
 
-
+ .search {
+   background: gray !important;
+    max-width: 500px;
+   
+  }
+  
+.titleStyle{
+  color:white;
+}
 </style>
