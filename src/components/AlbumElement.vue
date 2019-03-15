aad6f4f6cc1fe17c814798f8bd819f2e33b1eba9
Gabriel Brousseau <gabriel_brousseau@hotmail.com>
2019-03-15 16:05:03 -0400

    Add AlbumElement and AlbumElementList.

---
+++ src/components/AlbumElement.vue
@@ -0,0 +1,36 @@
+<template>
+  <md-card md-with-hover>
+    <md-card-media>
+      <img :src="img" alt="Album Cover">
+    </md-card-media>
+
+    <md-card-header>
+      <div class="md-title">Card with hover effect</div>
+      <div class="md-subhead">It also have a ripple</div>
+    </md-card-header>
+
+    <md-card-actions>
+      <md-button>Action</md-button>
+      <md-button>Action</md-button>
+    </md-card-actions>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'AlbumElement',
+  props: {
+    img: {
+      type: String,
+      required: false,
+      default: '/static/no-cover.jpg'
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .md-card {
+    width: 300px;
+  }
+</style>
---
+++ src/components/AlbumElementList.vue
@@ -0,0 +1,34 @@
+<template>
+  <md-content>
+    <album-element img="/static/album-cover.png"></album-element>
+    <album-element></album-element>
+    <album-element></album-element>
+    <album-element></album-element>
+    <album-element></album-element>
+    <album-element></album-element>
+  </md-content>
+</template>
+
+<script>
+import AlbumElement from '@/components/AlbumElement';
+
+export default {
+  components: {
+    'album-element': AlbumElement
+  }
+};
+</script>
+
+<style scoped>
+  .md-content {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    overflow: auto;
+  }
+
+  .md-card {
+    margin: 0.5rem;
+  }
+</style>
