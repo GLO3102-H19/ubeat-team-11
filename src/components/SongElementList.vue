f1db22c6dd95c6c35de9765d30c0e2921d996f90
Jacob Chaar <jacobchaar@live.ca>
2019-03-16 18:56:43 -0400

    add Song Template

---
+++ src/components/SongElementList.vue
@@ -0,0 +1,22 @@
+<template>
+       <md-table v-model="users" md-sort="trackName" md-sort-order="asc" md-card>
+      <md-table-toolbar>
+        <h1 class="md-title">Songs</h1>
+      </md-table-toolbar>
+      <song-element v-for="song in songs" v-bind:key="song.trackId" v-bind:song="songs"></song-element>
+    </md-table>
+</template>
+<script>
+import SongELement from '@/components/SongElement'
+export default {
+props:{
+    songs: Array
+},
+ components: {
+    'song-element': SongELement
+  }   
+};
+</script>
+<style>
+
+</style>
