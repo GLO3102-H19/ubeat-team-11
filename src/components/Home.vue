19703309e44c049b701557239ee6185012845786
Gabriel Brousseau <gabriel_brousseau@hotmail.com>
2019-03-16 18:42:22 -0400

    Adding PlaylistElement and PlaylistElementList.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <main>
-    <h1 class="md-display-1">New Albums</h1>
+    <h1 class="md-display-1">Play List</h1>
+    <playlist-element-list v-bind:playlists="newPlaylist"></playlist-element-list>
+    <h1 class="md-display-1">Albums</h1>
     <album-element-list v-bind:albums="newAlbums"></album-element-list>
-    <h1 class="md-display-1">Popular Albums</h1>
-    <album-element-list></album-element-list>
   </main>
 </template>
 
 <script>
 import AlbumElementList from '@/components/AlbumElementList';
+import PlaylistElementList from '@/components/PlaylistElementList';
 
 export default {
   components: {
-    'album-element-list': AlbumElementList
+    'album-element-list': AlbumElementList,
+    'playlist-element-list': PlaylistElementList
   },
   data: () => ({
     newAlbums: [
@@ -20,13 +22,23 @@ export default {
         id: 1,
         img: '/static/album-cover.png',
         title: 'Papillon',
-        artist: 'Penis'
+        artist: 'Island'
       },
       {
         id: 2,
         img: '/static/album-cover.png',
         title: 'Test2',
         artist: 'Joe Louis'
+      }
+    ],
+    newPlaylist: [
+      {
+        id: 1,
+        title: 'Test'
+      },
+      {
+        id: 2,
+        title: 'Test2'
       }
     ]
   })
---
+++ src/components/PlaylistElement.vue
@@ -0,0 +1,58 @@
+<template>
+  <md-card md-with-hover>
+    <md-card-media>
+        <img src="/static/no-cover.jpg" alt="Album Cover">
+        <md-button class="md-icon-button md-raised md-accent">
+          <md-icon >play_arrow</md-icon>
+        </md-button>
+    </md-card-media>
+
+    <md-card-header>
+      <md-card-header-text>
+        <div class="md-subheading">{{ playlist.title }}</div>
+        <div class="md-caption">Playlist</div>
+      </md-card-header-text>
+
+      <md-menu md-size="big" md-direction="bottom-end">
+        <md-button class="md-icon-button" md-menu-trigger>
+          <md-icon>more_vert</md-icon>
+        </md-button>
+
+        <md-menu-content>
+          <md-menu-item @click="">
+            <span>Delete playlist</span>
+          </md-menu-item>
+        </md-menu-content>
+      </md-menu>
+    </md-card-header>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'PlaylistElement',
+  props: {
+    playlist: {
+      type: Object,
+      required: true,
+      default: () => ({
+        id: 0,
+        title: 'Unknown'
+      })
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .md-card-media > .md-button {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin: 0.5rem;
+  }
+
+  .md-card {
+    width: 300px;
+  }
+</style>
