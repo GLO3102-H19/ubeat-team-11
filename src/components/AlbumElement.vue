83edc710d95b07bf9a65214ec972dd72da8493e5
Gabriel Brousseau <gabriel_brousseau@hotmail.com>
2019-03-16 15:27:22 -0400

    Implementing dynamic album creation.

--- src/components/AlbumElement.vue
+++ src/components/AlbumElement.vue
@@ -1,18 +1,16 @@
 <template>
   <md-card md-with-hover>
     <md-card-media>
-      <img :src="img" alt="Album Cover">
+        <img :src="album.img" alt="Album Cover">
+        <md-button class="md-icon-button md-raised md-accent">
+          <md-icon >play_arrow</md-icon>
+        </md-button>
     </md-card-media>
 
     <md-card-header>
-      <div class="md-title">Card with hover effect</div>
-      <div class="md-subhead">It also have a ripple</div>
+      <div class="md-subheading">{{ album.title }}</div>
+      <div class="md-caption">{{ album.artist }}</div>
     </md-card-header>
-
-    <md-card-actions>
-      <md-button>Action</md-button>
-      <md-button>Action</md-button>
-    </md-card-actions>
   </md-card>
 </template>
 
@@ -20,16 +18,28 @@
 export default {
   name: 'AlbumElement',
   props: {
-    img: {
-      type: String,
+    album: {
+      type: Object,
       required: false,
-      default: '/static/no-cover.jpg'
+      default: () => ({
+        id: 0,
+        img: '/static/no-cover.jpg',
+        title: 'Unknown',
+        artist: 'Unknown'
+      })
     }
   }
 };
 </script>
 
 <style scoped>
+  .md-card-media > .md-button {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin: 0.5rem;
+  }
+
   .md-card {
     width: 300px;
   }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
-    <h1 class="md-display-2">New Albums</h1>
-    <album-element-list></album-element-list>
-    <h1 class="md-display-2">Popular Albums</h1>
+    <h1 class="md-display-1">New Albums</h1>
+    <album-element-list v-bind:albums="newAlbums"></album-element-list>
+    <h1 class="md-display-1">Popular Albums</h1>
     <album-element-list></album-element-list>
   </main>
 </template>
@@ -13,13 +13,27 @@ import AlbumElementList from '@/components/AlbumElementList';
 export default {
   components: {
     'album-element-list': AlbumElementList
-  }
+  },
+  data: () => ({
+    newAlbums: [
+      {
+        id: 1,
+        img: '/static/album-cover.png',
+        title: 'Papillon',
+        artist: 'Penis'
+      },
+      {
+        id: 2,
+        img: '/static/album-cover.png',
+        title: 'Test2',
+        artist: 'Joe Louis'
+      }
+    ]
+  })
 };
 </script>
 
 
 <style scoped>
-  .md-content {
-  }
 </style>
 
