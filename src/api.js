5d0f22ff1ea9f0a62a70450b58558fac1cd620a3
Koriama <akoriama@gmail.com>
2019-03-16 23:48:33 -0400

    modify SongElement.vue and SongElementList.vue

--- src/api.js
+++ src/api.js
@@ -1,16 +1,19 @@
+import millisToMinutesAndSeconds from './utility';
+
 const url = 'http://ubeat.herokuapp.com/unsecure';
 
 export async function getAlbum(idAlbum) {
   const urlAlbum = `${url}/albums/${idAlbum}`;
   const response = await fetch(urlAlbum);
   const json = await response.json();
-  console.log(json);
   return {
     artistName: json.results[0].artistName,
     primaryGenreName: json.results[0].primaryGenreName,
     collectionName: json.results[0].collectionName,
+    copyright: json.results[0].copyright,
+    releaseDate: json.results[0].releaseDate,
+    collectionViewUrl: json.results[0].collectionViewUrl,
     artworkUrl100: json.results[0].artworkUrl100,
-
   };
 }
 
@@ -18,7 +21,6 @@ export async function getArtist(idArtist) {
   const urlArtist = `${url}/artists/${idArtist}`;
   const response = await fetch(urlArtist);
   const json = await response.json();
-  console.log(json);
   return {
     artistName: json.results[0].artistName,
     artistLinkUrl: json.results[0].artistLinkUrl,
@@ -33,12 +35,11 @@ export async function getTracks(idAlbum) {
   const urlTracks = `${url}/albums/${idAlbum}/tracks`;
   const response = await fetch(urlTracks);
   const json = await response.json();
-  console.log(json.results[0]);
   return {
     tracks: json.results.map(track => ({
       trackNumber: track.trackNumber,
       trackCensoredName: track.trackCensoredName,
-      trackTimeMillis: track.trackTimeMillis,
+      trackTimeMillis: millisToMinutesAndSeconds(track.trackTimeMillis),
       previewUrl: track.previewUrl,
     }))
 
