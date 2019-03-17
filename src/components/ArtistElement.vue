7cb3e48698b91daa68406e71ad34a8daba03cf6c
Jacob Chaar <jacobchaar@live.ca>
2019-03-17 18:17:20 -0400

    playlist in progress

---
+++ src/components/ArtistElement.vue
@@ -0,0 +1,40 @@
+<template>
+ <md-card>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">{{artist.artistName}}</div>
+        </md-card-header-text>
+
+        <md-card-media>
+          <md-avatar class="md-avatar-icon md-large">
+            <md-icon>personne_outline</md-icon>
+          </md-avatar>
+        </md-card-media>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button @click="">Details</md-button>
+      </md-card-actions>
+    </md-card>
+</template>
+<script>
+export default {
+    name: 'ArtistElement',
+    props:{
+        artist: {
+            type: Object,
+            required: true,
+            defaut: () => ({
+                artistId: 0,
+                artistName: 'Unknown',
+                primaryGenreName: 'Unknown',
+                artistLinkUrl: 'Unknown'
+            })
+        }
+    }
+}
+</script>
+<style>
+
+</style>
+
---
+++ src/components/PlaylistDialogCreate.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <md-dialog :md-active.sync="showDialog">
+      <md-dialog-title>Create a Playlist</md-dialog-title>
+  
+     
+      <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showDialog = false">Close</md-button>
+        <md-button class="md-primary" type="submit" @click="showDialog = false">Save</md-button>
+      </md-dialog-actions>
+
+    </md-dialog>
+  </div>
+</template>
+
+<script>
+ 
+
+export default {
+   name: 'PlaylistDialogCreate',
+   data: () => ({
+     name: '',
+     showDialog: false,
+     sending: false
+   })
+};
+</script>
+
+<style scoped>
+ .md-progress-bar {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+</style>
