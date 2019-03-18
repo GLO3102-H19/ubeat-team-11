95558a9dd6c171f8f131285a74d127807a22732c
Gabriel Brousseau <gabriel_brousseau@hotmail.com>
2019-03-17 22:34:38 -0400

    Added a new page and changed routing.

---
+++ src/components/Playlists.vue
@@ -0,0 +1,20 @@
+<template>
+  <md-app-content>
+    <p>Lorem Ipsum</p>
+  </md-app-content>
+</template>
+
+<script>
+import PlaylistElementList from '@/components/PlaylistElementList';
+
+export default {
+  name: 'Playlists',
+  components: {
+    'playlist-element-list': PlaylistElementList
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
