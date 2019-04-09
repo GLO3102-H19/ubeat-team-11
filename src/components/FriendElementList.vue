2c72736fe04000f7285a7703aa1ef8eb390c9ef1
Jacob Chaar <jacobchaar@live.ca>
2019-04-09 19:42:40 -0400

    finish the eveloppment of UserProfile and follow

---
+++ src/components/FriendElementList.vue
@@ -0,0 +1,26 @@
+<template>
+    <md-content>
+      <friend-element v-for="friend in friends" v-bind:key="friend.id" v-bind:friend="friend" v-on:update-follow="UpdateDialog"></friend-element>
+    </md-content>
+</template>
+<script>
+import FriendElement from '@/components/FriendElement';
+
+export default {
+  name: 'FriendElementList',
+  props: {
+    friends: Array,
+  },
+  components: {
+    'friend-element': FriendElement
+  },
+  methods: {
+    UpdateDialog(index, value) {
+      this.friends[index].doIfollow = value;
+    }
+  }
+};
+</script>
+<style scoped>
+
+</style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -4,12 +4,12 @@
       <div class="md-layout md-alignment-center-center">
         <div class="md-layout-item md-size-20 md-xsmall-size-100 md-small-100 md-medium-100">
             <md-avatar class="md-avatar-icon md-large md-accent">
-                  <md-ripple>{{this.initial}}</md-ripple>
+                  <md-ripple>{{this.userInformation.initial}}</md-ripple>
             </md-avatar>
         </div>
         <div class="md-layout-item md-size-40 md-xsmall-size-100 md-small-100 md-medium-100">
-          <h1 class="md-display-1">Name: {{this.name}}</h1>
-          <h1 class="md-display-1">Email: {{this.email}}</h1>
+          <h1 class="md-display-1">Name: {{this.userInformation.name}}</h1>
+          <h1 class="md-display-1">Email: {{this.userInformation.email}}</h1>
    
         </div>
         <div class="md-layout-item md-size-40 md-xsmall-size-100 md-small-100 md-medium-100">
@@ -24,34 +24,64 @@
 </template>
 <script>
 import PlaylistEmentList from '@/components/PlaylistElementList';
+import FriendElementList from '@/components/FriendElementList';
+import * as api from '../api';
 
 export default {
   name: 'UserProfile',
   data: () => ({
-    initial: '',
-    email: '',
-    name: '',
+    id: Number,
+    userInformation: Object,
     followStatusText: 'Unfollow',
     followStatus: Boolean,
-    playlistUsers: []
+    playlistUser: [],
+    friendlistUsers: []
   }),
   components: {
-    'playlist-element-list': PlaylistEmentList
+    'playlist-element-list': PlaylistEmentList,
+    'friend-element-list': FriendElementList
   },
-  mounted() {
+  async mounted() {
+    this.id = this.$route.params.id;
+    const itemUser = await api.getUsersbyId(this.id);
+    this.userInformation = itemUser;
+    const itemPlaylist = await api.getPlaylistById(this.id);
+    this.playlistUser.push(itemPlaylist);
     this.getIntial();
+    const connectedUser = await api.getTokenInfo();
+    let isInside = false;
+    connectedUser.following.forEach((element1) => {
+      itemUser.following.forEach((element2) => {
+        if (element1.id === element2.id) {
+          const item = element1;
+          item.doIfollow = true;
+          this.friendlistUsers.push(item);
+          isInside = true;
+        }
+      });
+      if (!isInside) {
+        const item = element1;
+        item.doIfollow = false;
+        this.friendlistUsers.push(item);
+      } else {
+        isInside = false;
+      }
+    });
   },
   methods: {
     getIntial() {
-      const editName = this.name.split(' ');
+      const editName = this.userInformation.name.split(' ');
       this.initial = editName[0].charAt(0) + editName[1].charAt(0);
     },
-    UpdateFollow() {
+    async UpdateFollow() {
       if (this.followStatus) {
-        // mettre API for follow someone
+        const result = await api.postFollow(this.id);
+        console.log(result);
         this.followStatusText = 'follow';
       } else {
           // mettre API for unfollow someone
+        const result = await api.deleteFollow(this.id);
+        console.log(result);
         this.followStatusText = 'unfollow';
       }
     }
